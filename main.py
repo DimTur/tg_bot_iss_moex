67b1a2f9cce7ce6ea82212abd13d8eb2cfcd6d4c
DimTur <tureckihdmitriq@gmail.com>
2023-09-15 11:24:04 +0300

    reformat code

--- main.py
+++ main.py
@@ -8,15 +8,6 @@ def get_url(secid: str):
     return f"https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/{secid}/.json?iss.only=marketdata"
 
 
-async def get_json_data(secid: str, session):
-    url = get_url(secid)
-    async with session.get(url) as response:
-        json_data = await response.json()
-
-    dict_data = await convert_to_dict(json_data)
-    return dict_data
-
-
 async def convert_to_dict(json_data: json, block_name="marketdata"):
     if json_data and block_name in json_data:
         list_of_dicts_securities = [
@@ -26,8 +17,13 @@ async def convert_to_dict(json_data: json, block_name="marketdata"):
     return list_of_dicts_securities
 
 
-async def get_shares_by_board_id():
-    pass
+async def get_json_data(secid: str, session):
+    url = get_url(secid)
+    async with session.get(url) as response:
+        json_data = await response.json()
+
+    dict_data = await convert_to_dict(json_data)
+    return dict_data
 
 
 async def main():
